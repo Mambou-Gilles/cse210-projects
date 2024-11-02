bcba9a5d4bf7dc70e5ddab2ac3a876be2e08a8c5
Mambou-Gilles <gillesravelmambou@gmail.com>
2024-11-02 16:00:45 +0200

    Prep 5 Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,35 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Welcome();
-        string name = Get_Name();
-        int num = Get_Favorite_Number();
-        int square = Square_Number(num);
-        Result(name, square);
+        display_welcome_message();
+        string name = get_user_name();
+        int num = get_user_favorite_number();
+        int square = square_root(num);
+        displayResult(name, square);
     }
-    static void Welcome()
+    static void display_welcome_message()
     {
         Console.WriteLine("Welcome to the program!");
     }
-    static string Get_Name()
+    static string get_user_name()
     {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
     }
-    static int Get_Favorite_Number()
+    static int get_user_favorite_number()
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
         return number;
     }
-    static int Square_Number(int number)
+    static int square_root(int number)
     {
         int square = (int)Math.Pow(number, 2);
+        // int square = number * number;
         return square;
     }
-    static void Result(string name, int square)
+    static void displayResult(string name, int square)
     {
-        Console.WriteLine($"{name}, the square of you number is {square}");
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
